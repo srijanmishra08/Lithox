31d27c2770d9d2cf15183821c1c0fa9a58ecb9af
Srijan Mishra <97506192+srijanmishra08@users.noreply.github.com>
2025-09-07 06:20:01 +0530

    Clean up: Remove unused imports for cleaner build

--- lib/screens/booking/booking_form_screen.dart
+++ lib/screens/booking/booking_form_screen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../services/email_service.dart';
 import '../../providers/tab_navigation_provider.dart';
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import '../providers/tab_navigation_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
